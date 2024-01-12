a4fcc530aa2eabd871a4779b3aee028233f503a0
Ben Frame <brewerytattooer@gmail.com>
2024-01-12 10:18:13 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -2,6 +2,13 @@
 const express = require('express') ; 
 const server = express() ; 
 
+server.get('/api/users', (req, res) =>{
+    console.log('getting all users')
+    res.status(200).json({
+        message: "working?"
+    })
+})
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: "not found"
